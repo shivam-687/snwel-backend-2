829997bd8c4df89c9e93749804909180706ccd2f
shivam-687 <shivamsi687@gmail.com>
2024-06-02 14:58:57 +0530

    update - 8

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,7 +5,7 @@ const logger = winston.createLogger({
   format: winston.format.json(),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'logs/combined.log' }),
+    // new winston.transports.File({ filename: 'logs/combined.log' }),
   ],
 });
 
