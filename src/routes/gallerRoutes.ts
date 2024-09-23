92f89311746fe550ea695d786cfc3e4d416b7570
shivam-687 <shivamsi687@gmail.com>
2024-09-23 06:46:43 +0530

    remove validation from gallery assets routes

--- src/routes/gallerRoutes.ts
+++ src/routes/gallerRoutes.ts
@@ -14,8 +14,8 @@ import passport from 'passport';
 const router = Router();
 
 router.post('/', validateSchema(createGalleryAssetSchema), createDigitalAssetController);
-router.get('/', passport.authenticate('jwt', { session: false }), getAllDigitalAssetsController);
-router.get('/:id', passport.authenticate('jwt', { session: false }), getDigitalAssetByIdController);
+router.get('/', getAllDigitalAssetsController);
+router.get('/:id', getDigitalAssetByIdController);
 router.put('/:id', passport.authenticate('jwt', { session: false }), validateSchema(updateGalleryAssetSchema), updateDigitalAssetByIdController);
 router.delete('/:id', passport.authenticate('jwt', { session: false }), deleteDigitalAssetByIdController);
 router.post('/bulk-upsert', passport.authenticate('jwt', { session: false }), validateSchema(createGalleryAssetSchema.array()), bulkUpsertDigitalAssetsController);
